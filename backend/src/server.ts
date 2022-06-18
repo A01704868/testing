dd4408fe878ff03e28ab36f686beefc6b15b67bd
A01704868 <A01704868@itesm.mx>
2022-06-18 10:15:05 -0500

    cors fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://http://159.223.174.63/:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
